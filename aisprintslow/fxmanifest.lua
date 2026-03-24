f818a489f9da9306d3149b3897035617cec68869
Pharma <34245836+logansportfolio@users.noreply.github.com>
2026-03-23 20:57:21 -0500

    Create fxmanifest.lua

---
+++ aisprintslow/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Logan L'
+description 'Slows AI ped sprint speed slightly'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server.lua'
+client_script 'client.lua'
